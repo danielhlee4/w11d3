76ae87cdd3d1412874a938ac0c7c2c2582e3045a
MudassarMemon <mudassar95memon@gmail.com>
2023-09-06 13:28:36 -0400

    Opps

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
@@ -1,3 +1,11 @@
+import produceData from "../mockData/produce.json";
+
+const POPULATE = "produce/POPULATE";
+
+export const populateProduce = (produce) => {
+  return { type: POPULATE, produce: produceData };
+};
+
 export default function produceReducer(state = {}, action) {
   switch (action.type) {
     default:

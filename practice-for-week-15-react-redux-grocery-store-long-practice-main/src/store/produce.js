11140c08c7df8fde1337440f7a151f2e0657e4e3
Dan Lee <danielhlee4@gmail.com>
2023-09-06 16:34:01 -0400

    add case populate in produceReducer

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/store/produce.js
@@ -2,13 +2,35 @@ import produceData from "../mockData/produce.json";
 
 const POPULATE = "produce/POPULATE";
 
-export const populateProduce = (produce) => {
+export const populateProduce = () => {
   return { type: POPULATE, produce: produceData };
 };
 
 export default function produceReducer(state = {}, action) {
+  Object.freeze(state);
+
+  const nextState = { ...state };
+
   switch (action.type) {
+    case POPULATE:
+      action.produce.forEach(element => {
+        nextState[element.id] = element;
+      });
+      return nextState;
     default:
-      return state;
+      return nextState;
   }
 }
+
+// export default function produceReducer(state = {}, action) {
+//   switch (action.type) {
+//     case POPULATE:
+//       const newState = {};
+//       action.produce.forEach(produce => {
+//         newState[produce.id] = produce;
+//       });
+//       return newState;
+//     default:
+//       return state;
+//   }
+// }
